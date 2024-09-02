69da1d9c348116419d9f24cdb08f2bd8103d0439
pk5ls20 <pk5ls20@outlook.com>
2024-09-03 05:52:19 +0800

    [App] Implementation part of navi ui

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,26 @@
 <template>
   <v-responsive class="border rounded">
     <v-app>
-      <v-app-bar :elevation="2">
-        <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
-        <v-app-bar-title>NocturneToKurage</v-app-bar-title>
+      <v-app-bar
+        :elevation="2"
+        prominent
+      >
+        <v-app-bar-nav-icon icon=menu variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>NocturneToKurage</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon="search" variant="text"></v-btn>
+        <v-btn icon="settings" variant="text"></v-btn>
+        <v-btn icon="more_vert" variant="text"></v-btn>
       </v-app-bar>
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+      >
+        <v-list
+          :items="items"
+          @click:select="console.log('click', $event)"
+        ></v-list>
+      </v-navigation-drawer>
       <v-container fluid>
         <v-row>
           <v-col cols="8">
@@ -25,4 +39,14 @@
 <script lang="ts" setup>
 import FlowTable from "@/components/FlowTable.vue";
 import DataDetailView from "@/components/DataDetailView.vue";
+
+import { ref } from 'vue'
+
+const drawer = ref(false)
+const items = ref([
+  {
+    title: 'qwq',
+    value: 'awa',
+  }
+])
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,19 +1,20 @@
-/**
- * plugins/vuetify.ts
- *
- * Framework documentation: https://vuetifyjs.com`
- */
-
-// Styles
 import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/styles'
-
-// Composables
 import { createVuetify } from 'vuetify'
+import {aliases} from "vuetify/iconsets/fa";
+import {md} from "vuetify/iconsets/md";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'dark',
+  },
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
   },
 })
